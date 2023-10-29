2a5b636a6e36dfb6047acafdc7cdfe2fa77b7a18
wpad1991 <wpad1991@gmail.com>
2023-10-29 16:52:43 +0900

    step7

---
+++ SwiftUIMemo/View/DetailView.swift
@@ -0,0 +1,41 @@
+//
+//  DetailView.swift
+//  SwiftUIMemo
+//
+//  Created by jisu kim on 10/29/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    @ObservedObject var memo: Memo
+    
+    @EnvironmentObject var store: MemoStore
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack {
+                        Text(memo.content)
+                            .padding()
+                        
+                        Spacer(minLength: 0)
+                    }
+                    
+                    Text(memo.insertDate, style: .date)
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("메모 보기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    MemoCell(memo: Memo(content: "Hello"))
+        .environmentObject(MemoStore())
+}
