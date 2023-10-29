a837b6c95b9750c883d119905dc768b684d60d7e
wpad1991 <wpad1991@gmail.com>
2023-10-29 15:01:46 +0900

    step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by jisu kim on 10/29/23.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
--- SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -13,7 +13,7 @@ struct SwiftUIMemoApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
