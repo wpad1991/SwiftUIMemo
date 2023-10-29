85062469d55eff600e2e53dab23fe64949da95bc
wpad1991 <wpad1991@gmail.com>
2023-10-29 15:13:49 +0900

    step2

---
+++ SwiftUIMemo/Memory/Memo.swift
@@ -0,0 +1,22 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by jisu kim on 10/29/23.
+//
+
+import Foundation
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+        id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+    }
+    
+}
