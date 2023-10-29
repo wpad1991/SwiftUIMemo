cc494ff7ea18059e954a110b9da5d0e3a29a24a9
wpad1991 <wpad1991@gmail.com>
2023-10-29 15:58:54 +0900

    step6

--- SwiftUIMemo/View/MainListView.swift
+++ SwiftUIMemo/View/MainListView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct MainListView: View {
     @EnvironmentObject var store: MemoStore
     
+    @State private var showComposer: Bool = false
+    
+    
     var body: some View {
         NavigationView {
             List(store.list) { memo in
@@ -17,6 +20,17 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+                
+            }
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+            }
         }
         
     }
